9ef6f4b04a0af1acad257d1436aa5187d62f2875
balastan <balasvn08@gmail.com>
2014-01-07 00:12:50 +0530

    gallery changes added

---
+++ app/models/gallery.rb
@@ -0,0 +1,5 @@
+class Gallery < ActiveRecord::Base
+
+  has_many :images
+  
+end
---
+++ app/models/image.rb
@@ -0,0 +1,7 @@
+class Image < ActiveRecord::Base
+	
+  belongs_to :gallery
+
+  mount_uploader :image, ImageUploader
+
+end
