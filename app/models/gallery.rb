98724575883ee120f4cae22c399a85d1ed45a457
Govindasamy <pkgobi@gmail.com>
2014-01-07 01:42:19 +0530

    conflict resolved

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,5 @@
 class Gallery < ActiveRecord::Base
-<<<<<<< HEAD
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"	
-end	
-=======
 
   has_many :images
   
 end
->>>>>>> 9ef6f4b04a0af1acad257d1436aa5187d62f2875
